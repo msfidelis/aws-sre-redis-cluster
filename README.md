6a9106e4b4aba9097e147c5fd77a293dec1dff62
Matheus Fidelis <matheus@nanoshots.com.br>
2021-09-17 23:15:00 -0300

    README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 * Cluster Mode
 * Encryption at rest
+* Automatic Failover
 * Multi-AZ 
 * Security Group / Access Compliance
 * Transit Encryption
@@ -43,4 +44,37 @@
 
 # Usage 
 
+```hcl
+module "compliance_redis" {
+    source = "github.com/msfidelis/aws-sre-redis-cluster"
+
+    name        = "app-x-redis"
+    node_type   = "cache.t2.micro"
+
+    vpc         = "vpc-ba8b92c1"
+
+    shards              = 2
+    replicas_per_shard  = 3
+
+    subnets     = [
+        "subnet-1198752f",
+        "subnet-c832eeaf",
+        "subnet-f634adf9"
+    ]
+
+    tags        = {
+        "environment" = "production"
+    }
+}
+```
+
 # Examples
+
+# Todo 
+
+* Prefix List support 
+* Auth user Support
+* Logs Support
+* Cloudwatch Metrics 
+* Cloudwatch Alarms 
+* SNS Topic for Alarms
--- security.tf
+++ security.tf
@@ -27,3 +27,26 @@ resource "aws_security_group_rule" "inbound_sgs" {
 
     security_group_id           = aws_security_group.main.id
 }
+
+resource "aws_security_group_rule" "outbound_cidrs" {
+    type                        = "egress"
+    from_port                   = 0
+    to_port                     = 0
+    protocol                    = "tcp"
+
+    cidr_blocks                 = length(var.cidrs_to_allow) > 0 ? var.cidrs_to_allow : data.aws_vpc.main.cidr_block_associations[*].cidr_block
+
+    security_group_id           = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "outbound_sgs" {
+    count                       = length(var.security_groups_to_allow)
+
+    type                        = "egress"
+    from_port                   = 0
+    to_port                     = 0
+    protocol                    = "tcp"
+    source_security_group_id    = var.security_groups_to_allow[count.index]
+
+    security_group_id           = aws_security_group.main.id
+}
